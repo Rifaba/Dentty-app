d11055753e2c7c46b5992eb6b06f9456f3767a81
Rifa Ba <153428213+Rifaba@users.noreply.github.com>
2025-03-28 15:08:59 +0800

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import { GoArrowUpRight } from "react-icons/go";
 
 export const Banner = () => {
   return (
-    <section className="bg-[url(/image1.png)] sm:flex bg-top  w-full h-screen bg-no-repeat bg-cover animate__animated animate__zoomInUp   ">
+    <section className="bg-[url(/image1.png)] sm:flex bg-top  w-full h-screen bg-no-repeat bg-cover   ">
       <div className="sm:flex grid  w-full items-center  gap-6 sm:justify-around sm:items-end px-4 py-5 sm:mt-15">
         <div className="grid gap-4 justify-between bg-slate-400/40 rounded-xl  text-justify p-5 ">
           <h4 className="font-semibold">Discount 5%</h4>
